fff3cd0a18efee531d30aaf487fca84128cf1598
Desuwa <Desuwaifu@gmail.com>
2013-08-26 18:06:37 -0700

    Update document.js

--- document.js
+++ document.js
@@ -1,16 +1,19 @@
-_addChatMessage = addChatMessage;
-addChatMessage = function(data){
-    // Save and restore these so this function has no side effects
-    username = data.username;
-    msg = data.msg;
-    if (data.username == "Naoko" && (match = data.msg.match(/^\((\S+)\) (.*)/))){
-        data.username = match[1] + "*";
-        data.msg = match[2];
+
+if (!_addChatMessage){
+    _addChatMessage = addChatMessage;
+    addChatMessage = function(data){
+        // Save and restore these so this function has no side effects
+        username = data.username;
+        msg = data.msg;
+        if (data.username == "Naoko" && (match = data.msg.match(/^\((\S+)\) (.*)/))){
+            data.username = match[1] + "*";
+            data.msg = match[2];
+        }
+        if (Math.floor(Math.random() * 10) == 0){
+            data.msg += " ~ de geso";
+        }
+        _addChatMessage(data);
+        data.username = username;
+        data.msg = msg;
     }
-    if (Math.floor(Math.random() * 10) == 0){
-        data.msg += " ~ de geso";
-    }
-    _addChatMessage(data);
-    data.username = username;
-    data.msg = msg;
 }
